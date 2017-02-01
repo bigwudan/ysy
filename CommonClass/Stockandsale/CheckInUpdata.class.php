b14a57f2498e4baaa7e612e6e461c6c38426d4a4
bigwudan <bigwudan@qq.com>
2017-02-01 17:36:04 +0800

    20170201 wudan -1

--- CommonClass/Stockandsale/CheckInUpdata.class.php
+++ CommonClass/Stockandsale/CheckInUpdata.class.php
@@ -41,10 +41,17 @@ class CheckInUpdata
         $Model = new \Think\Model();
         $Model->db()->startTrans();
         $flag = M('ysy_checkin')->add($this->_checkInGoods['checkin']);
-
         //checkingoods
         $flag = M('ysy_checkingoods')->addAll($this->_checkInGoods['checkingoods']);
-        $Model->db()->commit();
+        foreach($this->_stockList as $k => $v){
+
+            //$flag = M('ysy_stock')->where('format_id='.$v['where']['format_id'])->setInc('goods_num',$v['data']['goods_num']);
+            //$data = $Model->db()->query("select * from think_order where ordertime != 0 AND ordertime != 0 AND goodsname = '{$varGoodsName}' AND channel in ('个人','团购','渠道')");
+
+            var_dump($flag);
+            die();
+        }
+
         die();
 
 
