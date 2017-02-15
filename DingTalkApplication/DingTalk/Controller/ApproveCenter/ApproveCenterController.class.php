a7a0f1ce5919cbd3cd1b3f14aab37fd671514104
bigwudan <bigwudan@qq.com>
2017-02-15 21:44:38 +0800

    20170215 wudan

--- DingTalkApplication/DingTalk/Controller/ApproveCenter/ApproveCenterController.class.php
+++ DingTalkApplication/DingTalk/Controller/ApproveCenter/ApproveCenterController.class.php
@@ -73,11 +73,15 @@ EOT;
     public function actionApproveInfo(){
         $id = intval(I('id'));
         $orderList = M('ysy_order')->alias('yo')
-            ->field("yo.* , yog.* , ygp.packagename")
+
+            ->field("yo.order_id , yo.rece_name ,  yog.num , yog.price , yog.ordertype , u.realname , ygp.packagename")
+            ->join("think_user AS u ON u.id = yo.order_user")
             ->join('think_ysy_ordergoods AS yog ON yo.order_id = yog.order_id')
             ->join('think_ysy_goodspackage AS ygp ON yog.package_id = ygp.id')
             ->select();
 
+        var_dump($orderList);
+        die();
         $this->assign('order' , $orderList);
         $this->assign('orderid' , $id);
 
