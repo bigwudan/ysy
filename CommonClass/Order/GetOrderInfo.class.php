409a7b89398801db813b2cc082b1e54f4cefe8a1
bigwudan <bigwudan@qq.com>
2017-02-16 23:01:00 +0800

    20170216 wudan

---
+++ CommonClass/Order/GetOrderInfo.class.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/2/16
+ * Time: 21:24
+ */
+
+namespace CommonClass\Order;
+
+
+class GetOrderInfo
+{
+
+    /**
+     * orderId
+     */
+    private $_orderId = null;
+
+    /**
+     * 基础信息
+     */
+    private $_orderBase = null;
+
+    /**
+     * 详细信息
+     */
+    private $_orderInfo = null;
+
+    /**
+     * 初始化
+     * @param array $varOrderId
+     */
+    public function initi($varOrderId){
+        $this->_orderId = $varOrderId;
+        $orderList = $this->_runSql();
+        $this->_orderBase = $orderList['orderOfBase'];
+        $this->_orderInfo = $orderList['orderInfo'];
+
+    }
+
+    /**
+     * 运行SQL
+     */
+    public function _runSql(){
+        $orderList = M('ysy_order')->alias('yo')
+            ->field("yo.order_id , yo.rece_name , yo.addtime, yog.package_id , yog.num , yog.price , yog.ordertype , u.realname , ygp.packagename")
+            ->join("think_user AS u ON u.id = yo.order_user")
+            ->join('think_ysy_ordergoods AS yog ON yo.order_id = yog.order_id')
+            ->join('think_ysy_goodspackage AS ygp ON yog.package_id = ygp.id')
+            ->where("yo.order_id = {$this->_orderId}")
+            ->select();
+        if(!$orderList) return false;
+        $orderOfBase = reset($orderList);
+        $orderOfBase = array(
+            'orderId' => $orderOfBase['order_id'],
+            'receName' => $orderOfBase['rece_name'],
+            'addtime' => $orderOfBase['addtime'],
+            'realname' => $orderOfBase['realname'],
+        );
+        $orderInfo = array();
+        foreach($orderList as $k => $v){
+            $orderInfo[] = array(
+                'package_id' => $v['package_id'],
+                'num' => $v['num'],
+                'price' => $v['price'],
+                'ordertype' => $v['ordertype'],
+                'packagename' => $v['packagename']
+            );
+        }
+        foreach($orderInfo as $k => $v){
+            if($v['package_id']){
+                $data = M('ysy_goodspackage')->alias('gp')
+                    ->field('packagename , g.goods_id, num , goods_name')
+                    ->join("think_ysy_goodspackageinfo AS gpi ON gp.id = gpi.packageid")
+                    ->join("think_ysy_goods AS g ON g.goods_id = gpi.goods_id")
+                    ->where("gp.id = {$v['package_id']}")
+                    ->select();
+                $orderInfo[$k]['goodspackageinfo'] = $data;
+            }
+
+        }
+        return array('orderOfBase' => $orderOfBase , 'orderInfo' => $orderInfo);
+    }
+
+
+
+
+}
--- DingTalkApplication/DingTalk/Controller/ApproveCenter/ApproveCenterController.class.php
+++ DingTalkApplication/DingTalk/Controller/ApproveCenter/ApproveCenterController.class.php
@@ -72,16 +72,12 @@ EOT;
      */
     public function actionApproveInfo(){
         $id = intval(I('id'));
-        $orderList = M('ysy_order')->alias('yo')
 
-            ->field("yo.order_id , yo.rece_name ,  yog.num , yog.price , yog.ordertype , u.realname , ygp.packagename")
-            ->join("think_user AS u ON u.id = yo.order_user")
-            ->join('think_ysy_ordergoods AS yog ON yo.order_id = yog.order_id')
-            ->join('think_ysy_goodspackage AS ygp ON yog.package_id = ygp.id')
-            ->select();
+        $OrderInfo = new \CommonClass\Order\GetOrderInfo();
 
-        var_dump($orderList);
-        die();
+        $OrderInfo->initi($id);
+
+
         $this->assign('order' , $orderList);
         $this->assign('orderid' , $id);
 
